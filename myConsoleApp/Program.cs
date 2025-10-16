4ec00d0b773e865fab15934c268c2742a690688e
MiguelCeniceros <mceniceros@vistasolutions.ca>
2025-10-16 15:32:35 -0600

    Add initial files.

---
+++ myConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace myConsoleApp;
+
+/// <summary>
+/// This class represents a public class which should be documented.
+/// </summary>
+public class Another
+{
+    private int _myInt;
+
+    /// <summary>
+    /// Represents the value of the integer.
+    /// </summary>
+    public int MyInt
+    {
+        get => _myInt * 1;
+        set => _myInt = value;
+    }
+
+
+    /// <summary>
+    /// This method represents an action within the class. Should be documented too.
+    /// </summary>
+    public void DoSomething()
+    {
+        // do something
+        MyInt = 10;
+    }
+}
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello world");
+
+        var another = new Another();
+        another.DoSomething();
+
+    }
+
+
+}
