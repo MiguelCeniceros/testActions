c008977d555e8f53e5bc4bf4d1b9bb94e7c5aeb1
MiguelCeniceros <mceniceros@vistasolutions.ca>
2025-10-20 08:00:50 -0600

    update files

---
+++ myMain/OtherClass.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics.Contracts;
+
+namespace myMain {
+
+public class Hi
+{
+    public Hi()
+    {
+
+    }
+
+
+
+
+
+
+        public void DoSomething(string s)
+        {
+
+        }
+    
+
+
+
+
+    public void Hello(string a, string b)
+    {
+
+    }
+
+}
+}
+
+    
